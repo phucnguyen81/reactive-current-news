da130568f815f12e022b08c8c04ed05d6c5b5d8c
phucknguyen <phucknguyen@gmail.com>
2020-01-09 23:55:06 +0700

    Add light machine example

---
+++ src/app/learnxstate/light.machine.ts
@@ -0,0 +1,27 @@
+import { Machine, interpret } from 'xstate';
+
+const lightMachine = Machine({
+  id: 'light',
+  initial: 'green',
+  states: {
+    green: {
+      on: {
+        TIMER: 'yellow'
+      }
+    },
+    yellow: {
+      on: {
+        TIMER: 'red'
+      }
+    },
+    red: {
+      on: {
+        TIMER: 'green'
+      }
+    }
+  }
+});
+
+export function lightMachineService() {
+  return interpret(lightMachine);
+}
