14379eceeb058f7ebe52a67a90191e55e32cb5a7
phucknguyen <phucknguyen@gmail.com>
2020-01-09 16:22:44 +0700

    Clean up promise.machine

--- src/app/learnxstate/promise.machine.spec.ts
+++ src/app/learnxstate/promise.machine.spec.ts
@@ -1,7 +1,13 @@
-import { testPromiseMachine } from './promise.machine';
+import { promiseService } from './promise.machine';
 
 describe('promise machine', () => {
-  it('should work', () => {
-    testPromiseMachine();
+  it('should resolve on RESOLVE', () => {
+    const states = [];
+    const service = promiseService().onTransition(
+      state => states.push(state.value)
+    );
+    service.start();
+    service.send('RESOLVE');
+    expect(states).toEqual(['pending', 'resolved']);
   });
 });
--- src/app/learnxstate/promise.machine.ts
+++ src/app/learnxstate/promise.machine.ts
@@ -1,33 +1,24 @@
 import { Machine, interpret } from 'xstate';
 
-export function testPromiseMachine() {
-  const promiseMachine = Machine({
-    id: 'promise',
-    initial: 'pending',
-    states: {
-      pending: {
-        on: {
-          RESOLVE: 'resolved',
-          REJECT: 'rejected'
-        }
-      },
-      resolved: {
-        type: 'final'
-      },
-      rejected: {
-        type: 'final'
+const promiseMachine = Machine({
+  id: 'promise',
+  initial: 'pending',
+  states: {
+    pending: {
+      on: {
+        RESOLVE: 'resolved',
+        REJECT: 'rejected'
       }
+    },
+    resolved: {
+      type: 'final'
+    },
+    rejected: {
+      type: 'final'
     }
-  });
+  }
+});
 
-  const promiseService = interpret(promiseMachine).onTransition(state => {
-    console.log(state.value)
-  });
-
-  // Start the service
-  promiseService.start();
-  // => 'pending'
-
-  promiseService.send('RESOLVE');
-  // => 'resolved'
+export function promiseService() {
+  return interpret(promiseMachine);
 }
